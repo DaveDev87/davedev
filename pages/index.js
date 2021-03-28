4f8641ac3f5275e7b0ef2bcefcf87877b98b1c96
David Rueda <alexruedarivas@outlook.com>
2021-03-27 18:31:04 -0600

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,50 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import st from "../styles/Home.module.css";
 
 export default function Home() {
+  const {
+    container,
+    menu,
+    menuHeader,
+    icon,
+    menuTitle,
+    menuLinks,
+    title,
+    davedev,
+    softdev,
+  } = st;
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>DaveDev87</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        
+      <main className={container}>
+        <section className={menu}>
+          <div className={menuHeader}>
+            <div className={icon}>
+              <img src="/static/svg/menuicon.svg" alt="Menu icon" />
+            </div>
+            <div className={menuTitle}>
+              <span>Menu</span>
+            </div>
+          </div>
+          <div className={menuLinks}>
+            <a href="">Projects</a>
+            <a href="">Technologies</a>
+            <a href="">About Me</a>
+            <a href="">Contact</a>
+          </div>
+        </section>
+        <section>
+          <div className={title}>
+            <h1>
+              <span className={davedev}> DaveDev.</span> <br />
+              <span className={softdev}> Software Development. </span>
+            </h1>
+          </div>
+        </section>
       </main>
     </div>
-  )
+  );
 }
